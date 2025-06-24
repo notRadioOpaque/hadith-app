500501cf96b9f39e55e25a29186b552e91dc290b
abdulhakeem010 <bigmuak010@gmail.com>
2025-06-24 14:49:32 +0100

    Add a flex container to main.ts for improved layout of logos

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
+    <div class="flex gap-10">
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
+    </div>
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
