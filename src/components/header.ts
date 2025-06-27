20300233e7befdf942874c43edad194cc56d6765
abdulhakeem010 <bigmuak010@gmail.com>
2025-06-27 12:15:22 +0100

    feat: implement createHeader function and update layout
    Added a new createHeader function to generate a header element with child components. Updated the appLayout function to utilize the new header and modified the rendering logic accordingly. Cleaned up unused styles in style.css for better maintainability.

---
+++ src/components/header.ts
@@ -0,0 +1,10 @@
+export default function createHeader(children: HTMLElement[], className?: string): HTMLElement {
+  const header = document.createElement("header");
+  header.className = `border border-white flex justify-between  w-full px-4 py-2  bg-blue-600 text-white hover:bg-blue-700 !${className}`;
+
+  children.forEach((child) => {
+    header.appendChild(child);
+  });
+
+  return header;
+}
