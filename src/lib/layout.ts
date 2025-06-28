084bc04aeccd8ccc398b3623c995be6aa7cedcba
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-06-28 13:12:47 +0100

    feat: add footer to layout

--- src/lib/layout.ts
+++ src/lib/layout.ts
@@ -1,21 +1,17 @@
 import render from "./dom";
 import createHeader from "../components/header";
 import createFooter from "../components/footer";
-import createButton from "../components/button";
 import createMain from "../components/main";
-
 
 const header = createHeader();
 const main = createMain();
-
+const footer = createFooter();
 
 export default function appLayout() {
-  
   const div = document.createElement("div");
-  div.className = "px-8 py-6 flex flex-col gap-6";
+  div.className = "px-8 py-6 flex min-h-screen h-full flex-col gap-6";
 
-
-  render([header, main], div);
+  render([header, main, footer], div);
 
   return [div];
 }
