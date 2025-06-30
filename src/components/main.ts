c3225b0096c681fb37b9495ee082800b61786364
abdulhakeem010 <bigmuak010@gmail.com>
2025-06-30 10:39:51 +0100

    refactor: remove commented-out hero text functions from main component

--- src/components/main.ts
+++ src/components/main.ts
@@ -14,35 +14,3 @@ export default function createMain() {
   return main;
 }
 
-// function createHeroText() {
-//   const heroText = document.createElement("h1");
-//   heroText.textContent =
-//     "A Daily Hadith to Reflect, Remember, and Revive Your Soul";
-//   heroText.className = "w-[45%] p-4";
-
-//   return heroText;
-// }
-
-// function createSubHerotext() {
-//   const subHero = document.createElement("div");
-
-//   const subHeroText1 = document.createElement("h3");
-//   const subHeroText2 = document.createElement("h3");
-//   const subHeroText3 = document.createElement("h3");
-
-//   subHeroText1.textContent =
-//     "Discover the timeless wisdom of the Prophet ﷺ, one hadith at a time.";
-//   subHeroText2.textContent =
-//     "Whether you're starting your morning or seeking guidance at night, each hadith is a gentle reminder of faith, character, and purpose.";
-//   subHeroText3.textContent =
-//     "Stay connected. Stay inspired. One day, one hadith, every day.";
-
-//   subHero.className = "w-[55%] p-4";
-//   subHeroText1.className = "";
-//   subHeroText2.className = "";
-//   subHeroText3.className = "";
-
-//   render([subHeroText1, subHeroText2, subHeroText3], subHero);
-
-//   return subHero;
-// }
