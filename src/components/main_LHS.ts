ebcd14519bed41caa6050e0c84dc6c1345586677
abdulhakeem010 <bigmuak010@gmail.com>
2025-06-30 10:28:12 +0100

    feat: implement main LHS and RHS components with hero text

---
+++ src/components/main_LHS.ts
@@ -0,0 +1,21 @@
+import render from "../lib/dom";
+
+export default function createLHS() {
+  const div = document.createElement("div");
+  const heroText = createHeroText();
+  div.className = "border border-[#00a357] w-[45%]";
+
+  render([heroText,], div)
+
+  return div;
+}
+
+
+function createHeroText() {
+  const heroText = document.createElement("h1");
+  heroText.textContent =
+    "A Daily Hadith to Reflect, Remember, and Revive Your Soul";
+  heroText.className = "";
+
+  return heroText;
+}
