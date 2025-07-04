9f3f464409ae0435fd04e0bece33d56b78b63546
abdulhakeem010 <bigmuak010@gmail.com>
2025-07-04 09:28:21 +0100

    refactor: added additional styling to the subscribe modal

--- src/components/subscribe-modal.ts
+++ src/components/subscribe-modal.ts
@@ -37,16 +37,16 @@ function createContent() {
   const subscribeBtn = document.createElement("button");
 
   content.className =
-    "w-[50%] h-[80%]  rounded-lg flex items-center text-center flex-col bg-[#242124] p-20";
+    "w-[50%] h-[80%]  rounded-lg flex items-center justify-center  text-center flex-col bg-[#242124] p-20";
   contentTitle.className = "font-bold text-4xl ";
 
   contentSubtitle.className = "mt-6 font-semibold text-xl ";
 
   contentForm.className =
-    "w-full flex flex-col items-center justify-center  gap-10 mt-15 border border-[#F0FFF0] p-10 rounded-lg";
+    "w-full flex flex-col items-center justify-center gap-10 mt-10";
 
   emailInput.className =
-    "w-full max-w-sm px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ";
+    "w-full max-w-sm px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 ";
 
   subscribeBtn.className =
     "p-3 !w-[200px] !rounded-full hover:ring-[#F0FFF0] hover:ring-2 hover:ring-offset-3 hover:ring-offset-[#242124]  w-full !text-[#242124] !bg-[#F0FFF0] !font-bold !text-lg";
