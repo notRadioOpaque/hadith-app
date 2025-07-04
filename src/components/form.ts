48227657c6d85a72db47b9987ff8d3b2eaa28cea
abdulhakeem010 <bigmuak010@gmail.com>
2025-07-04 10:04:47 +0100

    refactor: integrate form creation into subscribe modal

---
+++ src/components/form.ts
@@ -0,0 +1,36 @@
+import render from "../lib/dom";
+
+export default function createForm() {
+  const form = document.createElement("form");
+
+  form.className =
+    "w-full flex flex-col items-center justify-center gap-10 mt-10";
+
+  render([createEmailInput(), createSubscribeBtn()], form);
+
+  return form;
+}
+
+function createEmailInput() {
+  const emailInput = document.createElement("input");
+
+  emailInput.className =
+  "w-full max-w-sm px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 ";
+  emailInput.type = "email";
+  emailInput.name = "email";
+  emailInput.id = "email";
+  emailInput.placeholder = "example@gmail.com";
+
+  return emailInput;
+}
+
+function createSubscribeBtn() {
+  const subscribeBtn = document.createElement("button");
+
+  subscribeBtn.className =
+    "p-3 !w-[200px] !rounded-full hover:ring-[#F0FFF0] hover:ring-2 hover:ring-offset-3 hover:ring-offset-[#242124]  w-full !text-[#242124] !bg-[#F0FFF0] !font-bold !text-lg";
+    subscribeBtn.type = "submit";
+    subscribeBtn.textContent = "Subscribe";
+
+    return subscribeBtn;
+}
--- src/components/subscribe-modal.ts
+++ src/components/subscribe-modal.ts
@@ -1,4 +1,5 @@
 import render from "../lib/dom";
+import createForm from "./form";
 
 export default function createSubscribeModal() {
   const modalContainer = document.createElement("div");
@@ -32,38 +33,16 @@ function createContent() {
   const content = document.createElement("div");
   const contentTitle = document.createElement("h2");
   const contentSubtitle = document.createElement("p");
-  const contentForm = document.createElement("form");
-  const emailInput = document.createElement("input");
-  const subscribeBtn = document.createElement("button");
+
 
   content.className =
     "w-[50%] h-[80%]  rounded-lg flex items-center justify-center  text-center flex-col bg-[#242124] p-20";
   contentTitle.className = "font-bold text-4xl ";
-
   contentSubtitle.className = "mt-6 font-semibold text-xl ";
-
-  contentForm.className =
-    "w-full flex flex-col items-center justify-center gap-10 mt-10";
-
-  emailInput.className =
-    "w-full max-w-sm px-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 ";
-
-  subscribeBtn.className =
-    "p-3 !w-[200px] !rounded-full hover:ring-[#F0FFF0] hover:ring-2 hover:ring-offset-3 hover:ring-offset-[#242124]  w-full !text-[#242124] !bg-[#F0FFF0] !font-bold !text-lg";
-
   contentTitle.textContent = "Title here";
   contentSubtitle.textContent = "Subtitle goes here";
-  emailInput.type = "email";
-  emailInput.name = "email";
-  emailInput.id = "email";
-  emailInput.placeholder = "example@gmail.com";
-  subscribeBtn.type = "submit";
-  subscribeBtn.textContent = "Subscribe";
 
-  contentForm.appendChild(emailInput);
-  contentForm.appendChild(subscribeBtn);
-
-  render([contentTitle, contentSubtitle, contentForm], content);
+  render([contentTitle, contentSubtitle, createForm()], content);
 
   return content;
 }
