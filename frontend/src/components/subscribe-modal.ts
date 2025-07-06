4fa4328fde5e82ace6445c9ac52ab9b30b100b8d
abdulhakeem010 <bigmuak010@gmail.com>
2025-07-06 22:01:22 +0100

    feat: add close button to subscribe modal

--- frontend/src/components/subscribe-modal.ts
+++ frontend/src/components/subscribe-modal.ts
@@ -1,6 +1,10 @@
 import render from "../lib/dom";
 import createForm from "./form";
+<<<<<<< HEAD
 import closeBtn from "../assets/icons/close-icon.svg"
+=======
+import closeBtn from "../assets/icons/close-icon.svg";
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
 
 export default function createSubscribeModal() {
   const modalContainer = document.createElement("div");
@@ -40,12 +44,20 @@ function createContent() {
 
   content.className =
 <<<<<<< HEAD
+<<<<<<< HEAD
     "w-[50%] h-[80%] rounded-lg flex items-center text-center flex-col bg-[#242124] relative";
   closeBtnDiv.className= "flex justify-end items-center absolute top-4 right-4";
   textAndFormDiv.className = "w-full flex flex-col flex-grow items-center justify-center"
 =======
     "w-[50%] h-[60%]  rounded-lg flex items-center justify-center  text-center flex-col bg-[#242124] p-20";
 >>>>>>> 753d4c7 (feat: subscribe logic)
+=======
+    "w-[50%] h-[80%] rounded-lg flex items-center text-center flex-col bg-[#242124] relative";
+  closeBtnDiv.className =
+    "flex justify-end items-center absolute top-4 right-4";
+  textAndFormDiv.className =
+    "w-full flex flex-col flex-grow items-center justify-center";
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
   contentTitle.className = "font-bold text-4xl ";
   contentSubtitle.className = "mt-6 font-semibold text-xl ";
   contentTitle.textContent = "Get Daily Hadith in Your Inbox";
@@ -55,14 +67,21 @@ function createContent() {
   closeBtnDiv.appendChild(closeBtn);
   [contentTitle, contentSubtitle, form].forEach((child) => {
     textAndFormDiv.appendChild(child);
+<<<<<<< HEAD
   })
+=======
+  });
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
 
   render([closeBtnDiv, textAndFormDiv], content);
 
   return content;
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
 function createCloseButton() {
   const closeButton = document.createElement("button");
   const btnIcon = document.createElement("img");
@@ -71,13 +90,25 @@ function createCloseButton() {
   btnIcon.className = "w-[40px] h-[40px] block";
 
   closeButton.className =
+<<<<<<< HEAD
     "flex !bg-transparent !px-2.5 gap-3";
 
     closeButton.addEventListener("click", () => {
       closeButton.closest(".show")?.classList.remove("show");
     });
+=======
+    "flex !bg-transparent !border-none hover:!border-none mt-0";
+
+  closeButton.addEventListener("click", () => {
+    closeButton.closest(".show")?.classList.remove("show");
+  });
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
 
   closeButton.appendChild(btnIcon);
 
   return closeButton;
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 22d7332 (feat: add close button to subscribe modal)
